4e08527fa1503d3874a1162fe1a6889d2d5d5bc1
shubrath <shubrathkg@gmail.com>
2024-12-22 15:41:28 +0530

    modified

---
+++ Assent/Assent/src/test/java/com/AssentProject/Assent/AssentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AssentProject.Assent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
