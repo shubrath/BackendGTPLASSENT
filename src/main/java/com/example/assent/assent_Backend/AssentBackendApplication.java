643451fed5f17b0ad597304109e4b18f7ae237f5
PavanMallepoola <pavan.kumar@ascenddefi.com>
2024-12-22 16:37:44 +0530

    first commit

---
+++ src/main/java/com/example/assent/assent_Backend/AssentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.assent.assent_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/assent/assent_Backend/AssentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assent.assent_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
